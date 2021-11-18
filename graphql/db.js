b6df3feb8ec2ac14ef9296fdbd2218a4afff894e
juo <hwgiwh271@gmail.com>
2021-11-19 02:19:43 +0900

    Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,18 +1,36 @@
-const people = [
-  { id: "1", name: "juo", age: 2, gender: "남자" },
-  { id: "2", name: "juo1", age: 3, gender: "여자" },
-  { id: "3", name: "juo2", age: 76, gender: "남자" },
-  { id: "4", name: "juo3", age: 6, gender: "여자" },
-  { id: "5", name: "juo4", age: 26, gender: "남자" },
-  { id: "6", name: "juo5", age: 22, gender: "여자" },
-  { id: "7", name: "juo6", age: 29, gender: "여자" },
-  { id: "8", name: "juo7", age: 16, gender: "남자" },
-  { id: "9", name: "juo8", age: 25, gender: "여자" },
-  { id: "10", name: "juo9", age: 46, gender: "남자" },
+let movies = [
+  { id: 1, name: "juo", score: 2 },
+  { id: 2, name: "juo1", score: 3 },
+  { id: 3, name: "juo2", score: 76 },
+  { id: 4, name: "juo3", score: 6 },
+  { id: 5, name: "juo4", score: 26 },
+  { id: 6, name: "juo5", score: 22 },
+  { id: 7, name: "juo6", score: 29 },
+  { id: 8, name: "juo7", score: 16 },
+  { id: 9, name: "juo8", score: 25 },
+  { id: 10, name: "juo9", score: 46 },
 ];
 
-const getById = (id) => {
-  const targets = people.filter((person) => person.id === id);
-  return targets[0];
+export const getMovies = () => movies;
+export const getById = (id) => {
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
 };
-export { people, getById };
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
+};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,12 @@
-import { people, getById } from "./db.js";
+import { getMovies, getById, addMovie, deleteMovie } from "./db.js";
 const resolvers = {
   Query: {
-    people: () => people,
-    person: (_, { id }) => getById(id),
+    movies: () => getMovies(),
+    movie: (_, { id }) => getById(id),
+  },
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
